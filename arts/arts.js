884ee227d1ffd13ccfcdfca5af1f1f94d9d3a95b
LemurPwned <jakub.mojsiejuk@gmail.com>
2022-08-09 00:16:46 +0200

    updates to match new template

--- arts/arts.js
+++ arts/arts.js
@@ -1,5 +1,5 @@
 // set the dimensions and margins of the graph
-var margin = { top: 10, right: 20, bottom: 20, left: 40 },
+var margin = { top: 10, right: 20, bottom: 20, left: 20 },
   width = 820 - margin.left - margin.right,
   height = 600 - margin.top - margin.bottom;
 
@@ -12,7 +12,7 @@ var svg = d3
   //   .append("g")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var zoomView = d3
+var zoomView = d3 
   .select("#imageTooltip")
   .append("svg")
   .attr("width", width / 2 + margin.left + margin.right)
@@ -43,7 +43,7 @@ zoomView
   .attr("height", 300)
   .style("opacity", 1.0);
 
-var mousemove = function(d) {
+var mousemove = function (d) {
   var labels = d.label;
   var labels = labels.replace(/,/g, "");
   labels = labels.replace(/\[/g, "");
@@ -58,7 +58,7 @@ var mousemove = function(d) {
 };
 
 //Read the data
-d3.csv("./arts/TSNE_ART_DICT_REVISED_SVD_CLUSTERING.csv", function(data) {
+d3.csv("/arts/TSNE_ART_DICT_REVISED_SVD_CLUSTERING.csv", function (data) {
   // Add X axis
   var max = 2.5;
   var x = d3
@@ -80,33 +80,33 @@ d3.csv("./arts/TSNE_ART_DICT_REVISED_SVD_CLUSTERING.csv", function(data) {
     .enter()
     .append("image")
     .attr("class", "point")
-    .attr("xlink:href", function(d) {
+    .attr("xlink:href", function (d) {
       return d.href;
     })
-    .attr("x", function(d) {
+    .attr("x", function (d) {
       return x(d.x);
     })
-    .attr("y", function(d) {
+    .attr("y", function (d) {
       return y(d.y);
     })
-    .attr("id", function(d) {
+    .attr("id", function (d) {
       return d.idx;
     })
-    .attr("lab", function(d) {
+    .attr("lab", function (d) {
       return d.label;
     })
     .attr("width", 32)
     .attr("height", 32)
     .style("opacity", 1.0)
 
-    .on("mouseover", function(d) {
+    .on("mouseover", function (d) {
       d3.selectAll(".point").style("opacity", 0.3);
       d3.select(this)
         .attr("width", 128)
         .attr("height", 128)
         .style("opacity", 1.0);
     })
-    .on("mouseleave", function(d) {
+    .on("mouseleave", function (d) {
       d3.selectAll("image").style("opacity", 1.0);
 
       d3.select(this)
@@ -114,7 +114,7 @@ d3.csv("./arts/TSNE_ART_DICT_REVISED_SVD_CLUSTERING.csv", function(data) {
         .attr("height", 32);
     })
     .on("mousemove", mousemove)
-    .on("click", function(d) {
+    .on("click", function (d) {
       d3.select(".zoomView").attr("xlink:href", d.href);
     });
 });
